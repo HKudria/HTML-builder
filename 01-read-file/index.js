2863dfcd0a89bfe325ba09941c14bd330ff50b58
Herman Kudria <h.kudrya@hotmail.com>
2022-11-01 15:01:20 +0100

    refactor: read file by createReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,15 @@
 const file = __dirname + `\\text.txt`;
 const fs = require('fs')
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+
+// fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+let readableStream = fs.createReadStream(file,'utf8')
+readableStream.on('data', data =>{
+    console.log(data)
+})
