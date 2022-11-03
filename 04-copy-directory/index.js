363d87c55b2c5796c06df4a7cac82495bd8fb4f1
Herman Kudria <h.kudrya@hotmail.com>
2022-11-03 11:19:32 +0100

    feat: task #4 copy file from one folder to another

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const { mkdir } = require('fs/promises');
+const dir = path.join(__dirname, 'files');
+const dirCopy = path.join(__dirname, 'files-copy');
 
+const copyFileFromFolder = () => {
+    fs.rm(dirCopy, {recursive: true, force: true}, () => {
+        mkdir(dirCopy, {recursive: true}).then(() => {
+            fs.readdir(dir, {withFileTypes: true}, (err, files,) => {
+                for (let file of files) {
+                    let fullName = path.join(dir, file.name);
+                    let fullNameCopy = path.join(dirCopy, file.name);
+                    fs.createReadStream(fullName).pipe(fs.createWriteStream(fullNameCopy));
+                }
+            });
+        });
+    });
+}
+
+copyFileFromFolder()
